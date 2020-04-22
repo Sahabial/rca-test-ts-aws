55757881b88c496966c432d8b39ed361641616e7
Anton Levkovich <sahabial@tut.y>
2020-04-22 19:51:42 +0300

    test travis integration

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React FASTER
+          Learn React FASTER AND FASTER
         </a>
       </header>
     </div>
