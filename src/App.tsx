a8577ee98aff22cfd13c1910a0135c8599288470
Anton Levkovich <sahabial@tut.y>
2020-04-23 10:54:33 +0300

    test cloudfront integration

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React FASTER AND FASTER
+          Learn React FASTER AND FASTER with Cloudfront
         </a>
       </header>
     </div>
