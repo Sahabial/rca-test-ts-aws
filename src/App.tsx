81b4a011a9f9f21882c27f3022a3f0504a28f9c5
Anton Levkovich <sahabial@tut.y>
2020-05-18 13:34:16 +0300

    cloudfront test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test disable travis
+          Cloudfront test
         </a>
       </header>
     </div>
